6995afd6edcac4057ce200c48eeb8cf25166a5dd
Tomasz Dmowski <dmowski.tomek@gmail.com>
2025-03-20 21:35:02 +0100

    feat: add nuxt-link with no prefetch

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,9 @@ useHead({
       :key="home.objectID"
       style="float: left; margin: 10px"
     >
-      <HomeCard :home="home" />
+      <nuxt-link :to="`/home/${home.objectID}`" no-prefetch>
+        <HomeCard :home="home" />
+      </nuxt-link>
     </div>
   </div>
 </template>
