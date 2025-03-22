31f7e2999b70bce1aab026c31beb5268668f4b9e
Tomasz Dmowski <dmowski.tomek@gmail.com>
2025-03-22 23:00:46 +0100

    feat: add error page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  error: { type: Object, required: true },
+});
+useHead({
+  title: 'Big Problem!',
+});
+</script>
+<template>
+    <div>
+      <h1>Error!</h1>
+        {{error.message}} {{error.statusCode}}
+    </div>
+</template>
